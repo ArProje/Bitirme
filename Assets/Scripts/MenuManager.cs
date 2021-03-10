2fddab6f9bd58b1b4f4ba7bd9b15d22b3332e7b5
BURCU PINARBASI <brcupnrbs@gmail.com>
2021-03-11 02:04:16 +0300

    first commit

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SinifBes()
+    {
+        SceneManager.LoadScene("Sınıf5Level");
+    }
+
+    public void SinifAlti()
+    {
+        SceneManager.LoadScene("Sınıf6Level");
+    }
+
+    public void SinifYedi()
+    {
+        SceneManager.LoadScene("Sınıf7Level");
+    }
+
+    public void SinifSekiz()
+    {
+        SceneManager.LoadScene("Sınıf8Level");
+    }
+
+    public void BaslaGeriDon()
+    {
+        SceneManager.LoadScene("BaslaLevel");
+    }
+
+
+}
